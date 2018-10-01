5b6922077d10dea747508d645ab2314e9dd9c727
roedoejet <aidanpine@shaw.ca>
2018-10-01 14:35:28 -0700

    xlsx stub

--- mtd/parsers/xlsx_parser.py
+++ mtd/parsers/xlsx_parser.py
@@ -1,38 +1,34 @@
 from openpyxl import load_workbook
 import pandas as pd
 from mtd.parsers.utils import BaseParser
+from mtd.exceptions import UnsupportedFiletypeError
 
 class Parser(BaseParser):
     '''
     Parse data for MTD **TODO
     '''
-    def __init__(self, manifest, resource):
+    def __init__(self, manifest, resource_path):
         self.manifest = manifest
-        work_book = load_workbook(resource)
-        work_sheet = work_book.active
-        if self.manifest['skip_first']:
-            min_row = 2
-        else:
-            min_row = 1
-        self.resource =  work_sheet.iter_rows(min_row=min_row)
-    
-    
+        try:
+            work_book = load_workbook(resource_path)
+            work_sheet = work_book.active
+            if self.manifest['skip_first']:
+                min_row = 2
+            else:
+                min_row = 1
+            self.resource = work_sheet.iter_rows(min_row=min_row)
+        except:
+            raise UnsupportedFiletypeError(f"File at {resource_path} not found.")
+        
+        self.entry_template = self.manifest['targets']
+
+    def getCellValue(self):
+        pass
     
     def resolve_targets(self):
         word_list = []
-        for entry in self.resource:
-            new_lemma = {}
-            for cell in entry:
-                for key, value in self.manifest['targets'].iteritems():
-                    if isinstance(value, str):
-                        if cell.column == value:
-                            new_lemma[key] = self.return_list(cell.value)
-                    elif isinstance(value, dict):
-                        new_lemma[key] = {}
-                        for inner_key, inner_value in value.iteritems():
-                            if cell.column == inner_value:
-                                new_lemma[key][inner_key] = cell.value
-            word_list.append(new_lemma)
+        for i, entry in enumerate(self.resource):
+            word_list.append(self.fill_entry_template(self.entry_template, entry, self.getCellValue))
         return word_list
 
     def parse(self):
