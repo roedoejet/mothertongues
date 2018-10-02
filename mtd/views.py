2555876db02bc4d39df1029eda12c45867baa9dd
roedoejet <aidanpine@shaw.ca>
2018-10-02 10:29:14 -0700

    added language resources api endpoint

--- mtd/views.py
+++ mtd/views.py
@@ -1,7 +1,11 @@
 from mtd.app import app
 import os
 from flask import render_template
+from mtd.resources import languages_api
+
+app.register_blueprint(languages_api, url_prefix='/api/v1')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
