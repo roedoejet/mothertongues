3c66cd10b26c0323443616a44a78f151eb5323c9
roedoejet <aidanpine@shaw.ca>
2019-10-03 21:05:09 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -6,17 +6,27 @@ import os
 build_no = dt.datetime.today().strftime('%Y%m%d')
 version_path = os.path.join(os.path.dirname(mtd.__file__), 'version.py')
 VERSION = mtd.VERSION + "." + build_no
+this_directory = os.path.abspath(os.path.dirname(__file__))
 
 with open(version_path, 'w') as f:
     f.write(f'__version__ = "{VERSION}"')
 
-with open('requirements.txt') as f:
+with open(os.path.join(this_directory, 'requirements.txt')) as f:
     REQS = f.read().splitlines()
 
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
+    author="Aidan Pine",
+    author_email="info@mothertongues.org",
     name='mothertongues',
     version=VERSION,
-    long_description='Mother Tongues',
+    license="AGPL-3.0",
+    url="https://github.com/roedoejet/mothertongues",
+    description='Mother Tongues Dictionaries dictionary creation tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
