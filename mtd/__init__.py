3cacfdc05402b1ddc88cd96ee04b5145fd50b42a
roedoejet <aidanpine@shaw.ca>
2020-04-11 11:57:52 -0700

    release(mtd): 0.16.*

--- mtd/__init__.py
+++ mtd/__init__.py
@@ -3,7 +3,7 @@ from mtd.languages.suites import LanguageSuite
 from mtd.dictionary import Dictionary
 from typing import List, Union
 
-VERSION = '0.15'
+VERSION = '0.16'
 
 def create_dictionary(language_config):
     """ Given a LanguageConfig (or a path to one), create a Dictionary
