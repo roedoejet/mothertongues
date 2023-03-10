4c489efe3b86cdf381a33b64be4971d212c4f2d5
roedoejet <hello@aidanpine.ca>
2023-03-09 17:13:28 -0800

    refactor(sorter): allow sorter to be configured ignore certain characters
    otherwise apply a heuristic to sort OOV characters last

--- mtd/processors/sorter.py
+++ mtd/processors/sorter.py
@@ -19,27 +19,42 @@ class ArbSorter(object):
         Args:
             order (list[str]): The order to sort by.
     '''
-    def __init__(self, order):
+    def __init__(self, order, ignorable=None):
+        self.ignorable = [] if ignorable is None else ignorable
         split_order = [re.escape(x) for x in sorted(order, key=len, reverse=True)]
-        self.splitter = re.compile(u"(%s)" % "|".join(split_order), re.UNICODE)
+        self.splitter = re.compile(f'({"|".join(split_order)})', re.UNICODE)
         # Next, collect weights for the ordering.
-        self.ords = {}
-        self.vals = []
-        for i in range(len(order)):
-            self.ords[order[i]] = i
-            self.vals.append(order[i])
+        self.char_to_ord_lookup = {order[i]: i for i in range(len(order))}
+        self.ord_to_char_lookup = {v: k for k, v in self.char_to_ord_lookup.items()}
+        self.oov_count = 10000
  
     # Turns a word into a list of ints representing the new
     # lexicographic ordering.  Python, helpfully, allows one to
     # sort ordered collections of all types, including lists.
     def word_as_values(self, word):
         """Turn word into values"""
-        word = self.splitter.split(word)[1::2]
-        return [self.ords[char] for char in word]
+        # ignore empty strings
+        word = [x for x in self.splitter.split(word) if x]
+        values = []
+        for char in word:
+            if char in self.ignorable:
+                continue
+            if char in self.char_to_ord_lookup:
+                values.append(self.char_to_ord_lookup[char])
+            else:
+                # OOV (can be multiple OOVs strung together)
+                for oov in char:
+                    if oov in self.ignorable:
+                        continue
+                    self.char_to_ord_lookup[oov] = self.oov_count
+                    self.ord_to_char_lookup[self.oov_count] = oov
+                    values.append(self.oov_count)
+                    self.oov_count += 1
+        return values
  
     def values_as_word(self, values):
         """Turn values into word"""
-        return "".join([self.vals[v] for v in values])
+        return "".join([self.ord_to_char_lookup[v] for v in values])
  
     def __call__(self, item_list, target):
         """Return sorted list based on item's (word's) sorting_form"""
